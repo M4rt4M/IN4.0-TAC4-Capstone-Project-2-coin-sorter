1feed9bc1b35b65146588f239d7914d3c1ae94d8
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-03-02 16:52:09 +0000

    Create 210302_code.py

---
+++ 210302_code.py
@@ -0,0 +1,19 @@
+##Multiple coin calc
+p=int(input('Please enter an amount of pennies between 0-1000:'))
+print(p//200, "£2")
+p = p%200
+print(p//100, "£1")
+p = p%100
+print(p//50, "50p")
+p = p%50
+print(p//20, "20p")
+p=p%20
+print(p//10, "10p")
+p=p%10
+print(p//1, "pennies")
+
+##One coin
+
+ 
+
+## Currency conversion
