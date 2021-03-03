b32902dcda14bbc485dca6d874bb1e72097e5868
Marta Majewska <47524327+M4rt4M@users.noreply.github.com>
2021-03-03 19:04:52 +0000

    Added in-line comments

--- part1.py
+++ part1.py
@@ -22,12 +22,12 @@ print("Here are available denominations: £2, £1, 50p, 20p, 10p")
 choice = "nothing"
 
 # Run
-while choice:
+while choice: #This while loop will run for as long as string <choice> is not empty
   choice=str(input("Please enter one denomination of choice: "))
-  if choice in coins:
+  if choice in coins: #This code will only run if user inputs one of pre-defined values
     single_coin(p, choice)
-    break
-  else:
+    break #Ends the while loop after 1 calculation
+  else: #This code will run if the value given by user is NOT in pre-defined list
     print("Sorry, you cannot choose this value.")
 
 #################################
@@ -50,11 +50,11 @@ def multiple_coins(p):
 p=int(input("\nPlease enter an amount of pennies between 0-10000: "))
 print("Here are available denominations: £2, £1, 50p, 20p, 10p")
 skip=str(input("\nPlease enter the value you would like to skip. If you don't choose anything, all coins will be displayed: "))
-if skip:
-  if skip in coins:
+if skip: #This code will runonly if user inputs a value (any)
+  if skip in coins: #This code will run only if the value given by user is in pre-defined list
     values.remove(values[coins.index(skip)])
     coins.remove(skip)
-  else:
+  else: #This code will run if the value given by user is NOT in pre-defined list
     print("\nSorry, you cannot skip this value")
 
 # Run calculator
@@ -65,8 +65,6 @@ multiple_coins(p)
 ########################
 
 ### What it does
-
-
 
 ####################
 # 4 - Set details  #
