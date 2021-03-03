e4ff5f7eb4269fe5b882d0d6950113f5c07ab975
josephfernandes <78435996+josephfernandes@users.noreply.github.com>
2021-03-03 22:55:38 +0000

    Update 210302_code.py

--- 210302_code.py
+++ 210302_code.py
@@ -2,7 +2,8 @@
 p=int(input('Please enter an amount of pennies between 0-1000:'))
 print(p//200, "£2")
 p = p%200
-print(p//100, "£1")
+print(p//100, 
+"£1")
 p = p%100
 print(p//50, "50p")
 p = p%50
@@ -11,9 +12,82 @@ p=p%20
 print(p//10, "10p")
 p=p%10
 print(p//1, "pennies")
-
 ##One coin
 
- 
 
-## Currency conversion
+print (30 * '-')
+print ("*** Coin Sorter - Main Menu ***")
+print (30 * '-')
+print ("1. Coin calulator")
+print ("2. Multiple Coin Calculator")
+print ("3. Print Coin List")
+print ("4. Set Details")
+print ("5. Display Program Configurations")
+print ("6. Quit The Program")
+print (30 * '-')
+
+Entry =int(input('Please enter an amount between 0-6:'))
+ 
+if Entry == 1:
+        print("coin calculator")
+        
+elif Entry == 2:
+         2==int(input('Please enter an amount of pennies between 0-1000:')
+         print(2//200, "£2")
+2 = 2%200
+print(2//100, "£1")
+2 = 2%100
+print(2//50, "50p")
+2 = 2%50
+print(2//20, "20p")
+2=2%20
+print(2//10, "10p")
+2=2%10
+print(2//1, "pennies")))
+        
+        
+
+elif Entry == 3:
+        print ("£2 equivalent to 200p")
+        print ("£1 equivalent to 100p")
+        print ("50p")
+        print ("20p")
+        print ("10p")
+        
+elif Entry == 4:
+     print("")
+        
+elif Entry == 5:
+    print("Display program configuration")
+
+elif Entry == 6:
+    print("Quit")
+       
+
+else:    ## default ##
+        print ("Invalid number. Try again...")
+
+        
+elif Entry == 2:
+         2==i
+        
+
+elif Entry == 3:
+        print ("£2 equivalent to 200p")
+        print ("£1 equivalent to 100p")
+        print ("50p")
+        print ("20p")
+        print ("10p")
+        
+elif Entry == 4:
+     print("")
+        
+elif Entry == 5:
+    print("Display program configuration")
+
+elif Entry == 6:
+    print("Quit")
+       
+
+else:    ## default ##
+        print ("Invalid number. Try again...")
